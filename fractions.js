876d7d4a844b9d8d84f6241ed11a1976bfc1b39c
Jaromir Obr <jaromir.obr@gmail.com>
2018-08-24 14:45:06 +0200

    Added a fraction case 0.1 + 0.2

--- fractions.js
+++ fractions.js
@@ -1,4 +1,6 @@
 const math = require('mathjs')
 
 a = math.add(math.fraction("-9.4'31'"), math.fraction("0.1'1'"));
+b = math.add(math.fraction("0.1"), math.fraction("0.2"));
 console.log(a);
+console.log(b);
