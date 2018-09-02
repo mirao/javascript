48dbdc62073cf70b2dab7fd271ca22f826968e46
Jaromir Obr <jaromir.obr@gmail.com>
2018-09-02 11:34:50 +0200

    Added own loop excerise

---
+++ eloquentjs/05_higher_order/own_loop.js
@@ -0,0 +1,13 @@
+function loop(value, testFunc, updateFunc, bodyFunc) {
+    for (let index = value; testFunc(index); index = updateFunc(index)) {
+        bodyFunc(index);
+    }
+}
+
+// Print 3, 2, 1
+loop(3, n => n > 0, n => n - 1, console.log);
+
+let a = [];
+// Create array [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+loop(0, n => n < 10, n => n + 1, val => a.push(val));
+console.log(a);
