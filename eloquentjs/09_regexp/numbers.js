9cafc2764e07e40bc9bbc4fbf156276e2ee0159c
Jaromir Obr <jaromir.obr@gmail.com>
2018-12-30 14:16:26 +0100

    Added regexp for JavaScript number

---
+++ eloquentjs/09_regexp/numbers.js
@@ -0,0 +1,16 @@
+// Regexp for JavaScript number
+let number = /^[-+]?(\d+\.?\d*|\.\d+)([eE][-+]?\d+)?$/;
+
+// Tests:
+for (let str of ["1", "156", "-1", "+15", "1.55", ".5", "5.",
+                 "1.3e2", "1E-4", "1e+12", "4e0", "+3.e1"]) {
+  if (!number.test(str)) {
+    console.log(`Failed to match '${str}'`);
+  }
+}
+for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+                 ".5.", "1f5", "."]) {
+  if (number.test(str)) {
+    console.log(`Incorrectly accepted '${str}'`);
+  }
+}
