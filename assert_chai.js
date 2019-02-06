19c94f7937252c31420027cae74e2555f0c77b98
Jaromir Obr <jaromir.obr@gmail.com>
2019-02-06 20:56:49 +0100

    Added explanations

--- assert_chai.js
+++ assert_chai.js
@@ -1,4 +1,4 @@
 require("chai").should();
 
-({a:1}).should.eql({a:1}, "no match");
-({a:1}).should.equal({a:1}, "no match");
+({a:1}).should.eql({a:1}, "no match"); // Matches because of deeply equal 
+({a:1}).should.equal({a:1}, "no match"); // Fails because of strict match using ===
