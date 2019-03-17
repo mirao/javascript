dc9d42313f3b6e079b67af5bf89ef37daaf86d1a
Jaromir Obr <jaromir.obr@gmail.com>
2019-03-17 14:04:32 +0100

    Added callback chaining of setTimeout

--- async/callback.js
+++ async/callback.js
@@ -1,7 +1,7 @@
 console.log("Starting..");
 
 function resolveAfter2Seconds() { 
-    const promise = new Promise(resolve => {
+    const promise = new Promise((resolve) => {
         setTimeout(() => {
             console.log("This runs after 2000 milliseconds.");
             resolve();
@@ -11,16 +11,28 @@ function resolveAfter2Seconds() {
 }
 
 (async () => {
-    // Wait 2 x 2 sec
+    // Wait 2 x 2 sec using await
     await resolveAfter2Seconds(); // Wait for 2 sec
-    console.log("End of waiting 1");
+    console.log("Awaiting 1");
     await resolveAfter2Seconds(); // Wait for 2 sec
-    console.log("End of waiting 2");
+    console.log("Awaiting 2");
 
     // Alternative waiting approach, again 2 x 2 sec
     resolveAfter2Seconds()
-        .then(() => { console.log("End of waiting 3"); return resolveAfter2Seconds();}) // Wait for 2 sec
-        .then(() => console.log("End of waiting 4")); // Wait for 2 sec
+        .then(() => { console.log("Promise waiting 1"); return resolveAfter2Seconds();}) // Wait for 2 sec
+        .then(() => console.log("Promise waiting 2")) // Wait for 2 sec
+        .then(() => 
+            // Another alternative using callbacks
+            setTimeout(function() {
+                console.log("Callback waiting 1");
+                setTimeout(function() {
+                    console.log("Callback waiting 2");
+                    setTimeout(function() {
+                        console.log("Callback waiting 3");
+                    }, 2000);
+                }, 2000);
+            }, 2000)
+        );
 })();
 
 console.log("Not really ending .."); // This one doesn't wait at all
